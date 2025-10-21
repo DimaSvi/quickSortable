98c3c79ef464e88404999e9e1a3b3c7c7ef80ead
VisionSpeed <dmytro.v.svitlyk@ukd.edu.ua>
2025-10-21 19:11:10 +0300

    Update quicksort.js

--- quicksort.js
+++ quicksort.js
@@ -1,25 +1,31 @@
-function quickSort(arr, ascending = true) {
-  if (arr.length <= 1) return arr;
+function quickSortInPlace(arr, ascending = true, low = 0, high = arr.length - 1) {
+  if (low < high) {
+    const pivotIndex = partition(arr, ascending, low, high);
+    quickSortInPlace(arr, ascending, low, pivotIndex - 1);
+    quickSortInPlace(arr, ascending, pivotIndex + 1, high);
+  }
+  return arr;
+}
 
-  const pivot = arr[arr.length - 1];
-  const left = [];
-  const right = [];
+function partition(arr, ascending, low, high) {
+  const pivot = arr[high];
+  let i = low;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (ascending ? arr[i] < pivot : arr[i] > pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+  for (let j = low; j < high; j++) {
+    if (ascending ? arr[j] < pivot : arr[j] > pivot) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
     }
   }
 
-  return [...quickSort(left, ascending), pivot, ...quickSort(right, ascending)];
+  [arr[i], arr[high]] = [arr[high], arr[i]];
+  return i;
 }
 
 const numbers = [5, 3, 8, 4, 2, 7, 1, 10];
 
 console.log("Сортування за зростанням:");
-console.log(quickSort(numbers, true));
+console.log(quickSortInPlace([...numbers], true));
 
 console.log("Сортування за спаданням:");
-console.log(quickSort(numbers, false));
+console.log(quickSortInPlace([...numbers], false));
